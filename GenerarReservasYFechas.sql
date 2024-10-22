300c29d9e11f65369f2c5d2f8031b91b8d374bd2
Nicolas <nicoezeperez@gmail.com>
2024-10-21 22:36:59 -0300

    comentado reservas y fechas para mas claridad

--- GenerarReservasYFechas.sql
+++ GenerarReservasYFechas.sql
@@ -12,44 +12,51 @@ DECLARE @Noches_minimas INT;
 DECLARE @Fecha_reservada DATE
 DECLARE @Id_reserva INT = 1;
 
+-- cantidad de reservas a generar
 WHILE (@cantidadReservas > 0)
 BEGIN
 	
+	-- elige un id de propiedad random
 	SET @Id_propiedad = (
 		SELECT TOP 1 Id_propiedad
 		FROM Propiedad
 		ORDER BY NEWID()
 	)
 
+	-- la fecha de hoy menos 0 a 365 dias
 	SET @Fecha_efectuada = DATEADD(DAY, -FLOOR(RAND() * 365), GETDATE());
 	
+	-- inserta la reserva
 	INSERT INTO Reserva (Id_propiedad, Id_usuario, Fecha_efectuada)
-	VALUES (
-		@Id_propiedad,
-		@cantidadReservas,
-		@Fecha_efectuada
-	);
+	VALUES (@Id_propiedad, @cantidadReservas, @Fecha_efectuada);
 
+	-- saca las noches minimas de la propiedad reservada
 	SET @Noches_minimas = (
 		SELECT TOP 1 Noches_minimas
 		FROM Propiedad
 		WHERE Id_propiedad = @Id_propiedad
 	);
 
-	SET @Fecha_reservada = DATEADD(DAY, FLOOR(RAND() * 30), @Fecha_efectuada);
+	-- establece la reservada mas 1 a 31 dias como primera fecha
+	SET @Fecha_reservada = DATEADD(DAY, 1 + FLOOR(RAND() * 30), @Fecha_efectuada);
 
+	-- hace tantas fechas como noches minimas acepta la propiedad
 	WHILE (@Noches_minimas > 0)
 	BEGIN
 
+		-- inserta la fecha 
 		INSERT INTO Fecha_reservada (Id_reserva, Fecha)
 		VALUES (@Id_reserva, @Fecha_reservada);
 
+		-- le suma uno a la fecha
 		SET @Fecha_reservada = DATEADD(DAY, 1, @Fecha_reservada);
 
+		-- disminuye las noches para el while
 		SET @Noches_minimas -= 1;
 
 	END;
 
+	-- disminuye y aumenta para el while y para el id de reserva
 	SET @cantidadReservas -= 1;
 	SET @Id_reserva += 1;
 
