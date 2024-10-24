042443b7b15ee5a270e9c00f6cc63709377093ec
AxelFenske <yo.axel48@gmail.com>
2024-10-24 16:58:00 -0300

    cambios generales?

--- p_reporte1.sql
+++ p_reporte1.sql
@@ -1,32 +1,32 @@
 WITH
 Propiedad_espacio AS (
-	SELECT Cama.Id_propiedad, SUM(Tipo_de_cama.Espacio) AS Espacio
-	FROM Cama
-	INNER JOIN Tipo_de_cama
-	ON Cama.Id_tipo_de_cama = Tipo_de_cama.Id_tipo_de_cama
-	GROUP BY Cama.Id_propiedad
+	SELECT C.Id_propiedad, SUM(TC.Espacio) AS Espacio
+	FROM Cama C
+	INNER JOIN Tipo_de_cama TC
+	ON C.Id_tipo_de_cama = TC.Id_tipo_de_cama
+	GROUP BY C.Id_propiedad
 ),
 Propiedad_dormitorio_banio AS (
-	SELECT Dormitorio.Id_propiedad, COUNT(Dormitorio.Numero) AS Dormitorios, Propiedad_banio.Banios
-	FROM Dormitorio
+	SELECT D.Id_propiedad, COUNT(D.Numero) AS Dormitorios, Propiedad_banio.Banios
+	FROM Dormitorio D
 	INNER JOIN (
-		SELECT Banio.Id_propiedad, COUNT(Banio.Numero) AS Banios
-		FROM Banio
-		GROUP BY Banio.Id_propiedad
-	) AS Propiedad_banio
-	ON Dormitorio.Id_propiedad = Propiedad_banio.Id_propiedad
-	GROUP BY Dormitorio.Id_propiedad, Propiedad_banio.Banios
+		SELECT B.Id_propiedad, COUNT(B.Numero) AS Banios
+		FROM Banio B
+		GROUP BY B.Id_propiedad
+	) AS Propiedad_banio 
+	ON D.Id_propiedad = Propiedad_banio.Id_propiedad
+	GROUP BY D.Id_propiedad, Propiedad_banio.Banios
 ),
 Propiedad_cama AS (
 	SELECT Id_propiedad, COUNT(Numero_cama) AS Camas
-	FROM Cama
+	FROM Cama C
 	GROUP BY Id_propiedad
 )
-SELECT Propiedad.Nombre, Propiedad_dormitorio_banio.Banios, Propiedad_dormitorio_banio.Dormitorios, Propiedad_cama.Camas, Propiedad_espacio.Espacio
-FROM Propiedad
-INNER JOIN Propiedad_espacio
-ON Propiedad.Id_propiedad = Propiedad_espacio.Id_propiedad
-INNER JOIN Propiedad_dormitorio_banio
-ON Propiedad_dormitorio_banio.Id_propiedad = Propiedad.Id_propiedad
-INNER JOIN Propiedad_cama
-ON Propiedad_cama.Id_propiedad = Propiedad.Id_propiedad
+SELECT P.Nombre, PDB.Banios, PDB.Dormitorios, PC.Camas, PE.Espacio
+FROM Propiedad P
+INNER JOIN Propiedad_espacio PE
+ON P.Id_propiedad = PE.Id_propiedad
+INNER JOIN Propiedad_dormitorio_banio PDB
+ON PDB.Id_propiedad = P.Id_propiedad
+INNER JOIN Propiedad_cama PC
+ON PC.Id_propiedad = P.Id_propiedad
