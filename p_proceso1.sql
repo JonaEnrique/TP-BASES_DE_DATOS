768c3d575841d3d53c621d7e2787ba203324ba3b
Nicolas <nicoezeperez@gmail.com>
2024-10-24 17:29:25 -0300

    proceso 1

---
+++ p_proceso1.sql
@@ -0,0 +1,69 @@
+CREATE OR ALTER PROCEDURE p_proceso1
+	@Accion CHAR,
+	@Id_usuario INT,
+	@Nombre VARCHAR(100) = NULL,
+	@Contrasenia VARCHAR(20) = NULL,
+	@Categoria INT = NULL
+AS
+BEGIN
+	IF (@Accion = 'A')
+	BEGIN
+		IF (NOT EXISTS (SELECT 1 FROM Usuario WHERE Id_usuario = @Id_usuario))
+		BEGIN
+			IF (dbo.f_funcion2(@Contrasenia) = 1)
+			BEGIN
+				IF (@Categoria IS NULL)
+				BEGIN
+					INSERT INTO Usuario
+					VALUES (@Id_usuario, @Nombre, @Contrasenia, @Categoria)
+					
+					PRINT ('Usuario nuevo agregado')
+				END
+				ELSE
+					PRINT ('la categoria debe ser nula al crear un usuario')
+			END
+			ELSE
+				PRINT ('contrasenia invalida')
+		END
+		ELSE
+			PRINT ('El usuario con ID ' + CAST(@Id_usuario AS VARCHAR(100)) + ' ya existe')
+	END
+	ELSE IF (@Accion = 'B')
+	BEGIN
+		IF (EXISTS (SELECT 1 FROM Usuario WHERE Id_usuario = @Id_usuario))
+		BEGIN
+			DELETE FROM Usuario
+			WHERE Id_usuario = @Id_usuario
+
+			PRINT ('El usuario con id ' + CAST(@Id_usuario AS VARCHAR(100)) + ' fue eliminado')
+		END
+		ELSE
+			PRINT ('El usuario con id ' + CAST(@Id_usuario AS VARCHAR(100)) + ' no existe')
+	END
+	ELSE IF (@Accion = 'M')
+	BEGIN
+		IF (EXISTS (SELECT 1 FROM Usuario WHERE Id_usuario = @Id_usuario))
+		BEGIN
+
+			IF (@Nombre IS NOT NULL AND
+				dbo.f_funcion2(@Contrasenia) = 1 AND
+				@Categoria IN (1, 2, NULL))
+				UPDATE Usuario
+				SET
+					Nombre = @Nombre,
+					Contrasenia = @Contrasenia,
+					Id_categoria = @Categoria
+				WHERE Id_usuario = @Id_usuario
+
+			PRINT ('El usuario con id ' + CAST(@Id_usuario AS VARCHAR(100)) + ' fue modificado')
+		END
+		ELSE
+			PRINT ('El usuario con id ' + CAST(@Id_usuario AS VARCHAR(100)) + ' no existe')
+	END
+END
+
+--EXEC p_proceso1 'A', 101, 'Nicolas', 'P@55word', NULL
+--EXEC p_proceso1 'B', 101
+--EXEC p_proceso1 'M', 101, 'Ezequiel', 'P4ssw@rd', 1
+
+--SELECT * FROM Usuario WHERE Id_usuario = 101
