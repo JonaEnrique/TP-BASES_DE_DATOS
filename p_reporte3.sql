6a146d0738850639c0fd71cdb162d30857d7f2f8
AxelFenske <yo.axel48@gmail.com>
2024-10-25 00:08:40 -0300

    reporte 3

---
+++ p_reporte3.sql
@@ -0,0 +1,36 @@
+--Reporte 3 --BUSCA HABITACIONES DIPONIBLES, le paso las fechas que necesito, y la cantidad de lugares
+--fecha inicio, fecha fin y lugares necesarios
+
+--DROP PROCEDURE p_reporte3
+CREATE PROCEDURE p_reporte3
+    @FechaInicio DATE,
+    @FechaFin DATE,
+    @lugares INT
+AS
+BEGIN
+  -- Tabla temporal para almacenar resultados de p_reporte1
+    CREATE TABLE #Reporte  (Id_propiedad INT,Nombre NVARCHAR(255),Banios INT,Dormitorios INT,Camas INT,Espacio INT);
+
+    -- Insertar los resultados de p_reporte1 en la tabla temporal
+    INSERT INTO #Reporte (Nombre, Banios, Dormitorios, Camas, Espacio, Id_propiedad)
+    EXEC p_reporte1;
+
+    -- Ahora filtra las propiedades en función de las reservas y el espacio necesario
+    SELECT R.Nombre, R.Banios, R.Dormitorios, R.Camas, R.Espacio, R.Id_propiedad
+    FROM #Reporte R
+    WHERE R.Espacio >= @lugares
+    AND R.Id_propiedad NOT IN (
+        SELECT R.Id_propiedad
+        FROM Reserva R
+        INNER JOIN Fecha_reservada FR ON R.Id_reserva = FR.Id_reserva
+        WHERE FR.fecha BETWEEN @FechaInicio AND @FechaFin
+    );
+
+    -- Eliminar la tabla temporal
+    DROP TABLE #Reporte;
+END;
+
+exec p_reporte3(
+
+select *
+from Reserva
